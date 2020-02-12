3231bd81d127ca74422b367cb4b4d0504c682516
xwy <2322968381@qq.com>
2020-02-12 14:22:20 +0800

    ListView entire line clicking

--- app/src/main/java/com/unzipper/android/MainActivity.java
+++ app/src/main/java/com/unzipper/android/MainActivity.java
@@ -26,25 +26,26 @@ public class MainActivity extends AppCompatActivity {
         fileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int num = fileListView.getSelectedItemPosition();
                 File file = fileList.get(position);
-                Toast.makeText(MainActivity.this, file.getName(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void initFiles(){
         for(int i = 0; i < 2; i++){
-            File apple = new File("Apple", R.drawable.fox);
+            File apple = new File("Apple", R.drawable.folder);
             fileList.add(apple);
-            File banana = new File("Banana", R.drawable.fox);
+            File banana = new File("Banana", R.drawable.folder);
             fileList.add(banana);
-            File orange = new File("Orange", R.drawable.fox);
+            File orange = new File("Orange", R.drawable.folder);
             fileList.add(orange);
-            File watermelon = new File("Watermelon", R.drawable.fox);
+            File watermelon = new File("Watermelon", R.drawable.folder);
             fileList.add(watermelon);
-            File pear = new File("Pear", R.drawable.fox);
+            File pear = new File("Pear", R.drawable.folder);
             fileList.add(pear);
-            File grape = new File("Grape", R.drawable.fox);
+            File grape = new File("Grape", R.drawable.folder);
             fileList.add(grape);
         }
     }
